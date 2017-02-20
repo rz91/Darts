af01dda6647d3e78f79ae2ab208f6520c44662fc
rz91 <ronny.zingler@gmail.com>
2017-02-20 22:02:03 +0100

    idea

---
+++ src/Launcher.java
@@ -0,0 +1,10 @@
+
+class Launcher {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        StartDialog main = new StartDialog();
+
+    }
+}
---
+++ src/StartDialog.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+/**
+ * Created by Ronny on 20.02.2017.
+ */
+public class StartDialog extends JFrame{
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JButton startGameButton;
+    private JPanel panel1;
+
+public StartDialog(){
+    setContentPane(panel1);
+    pack();
+    setVisible(true);
+}
+}
